ed54f40675f1bd644ef983caefc52f15a9c3cdb5
Wallace <wallacj@americas.corpdir.net>
2018-08-03 13:39:39 -0700

    fixed chal3 to use new crack interface

--- src/chal3.c
+++ src/chal3.c
@@ -11,13 +11,14 @@ int main()
 {
     size_t ct_size = str2bytes(cyphertext, &ct_bytes);
     
-    uint8_t key = byte_xor_crack(ct_bytes, ct_size);
+    struct byte_xor_chunk key = byte_xor_crack(ct_bytes, ct_size);
 
-    printf("%02x: ", key);
-    for (int i=0; i < ct_size; i++)
-        putc(ct_bytes[i] ^ key, stdout);
+    printf("%02x: ", key.key);
+    for (int i=0; i < key.pt_size; i++)
+        putc(key.plaintext[i], stdout);
     putc('\n', stdout);
     
+    free(key.plaintext);
     free(ct_bytes);
     return 0;
 }

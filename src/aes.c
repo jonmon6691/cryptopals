411b73a6e74e71aa90d210f12b464edd3f1dfa7b
Wallace <wallacj@americas.corpdir.net>
2018-08-15 09:05:08 -0700

    mix cols working

--- src/aes.c
+++ src/aes.c
@@ -73,6 +73,7 @@ uint32_t aes_key_schedule(unsigned int round)
 
 union block *dref(struct aes_state *s)
 {
+    // Convinience function, maybe should be a macro?
     return &s->state[s->si];
 }
 
@@ -84,14 +85,35 @@ void aes_sub_bytes(struct aes_state * s)
 
 void aes_shift_rows(struct aes_state * s)
 {
+    // Swap buffers since this won't be done in-place
     uint8_t *start = dref(s)->bytes;
     s->si = !s->si;
     uint8_t *end = dref(s)->bytes;
-    
+    // Shift each row to the left by n bytes where n is it's own row index
     for (int row = 0; row < 4; row++) {
         for (int col = 0; col < 4; col++) {
             // end[row * 4 + col]
-            end[row*4 + col] = start[row*4 + (row + col) % 4];
+            end[col*4 + row] = start[row + ((row + col) % 4) * 4];
+        }
+    }
+}
+
+void aes_mix_cols(struct aes_state * s)
+{
+    // swap buffers since this won't be done in-place
+    uint8_t *start = dref(s)->bytes;
+    s->si = !s->si;
+    uint8_t *end = dref(s)->bytes;
+    /* Multiply each column by the static polynomial in GF(2^8) */
+    uint8_t mix_cols_poly[4] = {2, 3, 1, 1};
+    for (int col = 0; col < 4; col++) {
+        for (int row = 0; row < 4; row++) {
+            end[4*col+row] = 0;
+            /* Matrix multiply, right-shifting the polynomial and computing the dot product with
+                the column repeatedly adding it back into the result column */
+            for (int row2 = 0; row2 < 4; row2++) {
+                end[4*col+row] ^= aes_mult(mix_cols_poly[(row2 - row + 4) % 4], start[4*col+row2]);
+            }
         }
     }
 }
@@ -100,22 +122,26 @@ void aes_print_block(struct aes_state * s)
 {
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            printf("%hhx ", s->state[s->si].grid[j][i]);
+            printf("%hhx ", dref(s)->grid[j][i]);
         }
         printf("\n");
     }
-
 }
 
 int main()
 {
     struct aes_state s;
     s.si = 0;
-    for (int i = 0; i < 16; i++) s.state[s.si].bytes[i] = i;
+    for (int i = 0; i < 16; i++) dref(&s)->bytes[i] = 0xd4;
+    dref(&s)->words[0] = 0x4c31262d;
+    dref(&s)->words[1] = 0x455313db;
+    dref(&s)->words[2] = 0x5c220af2;
+    aes_print_block(&s);
+    printf("\n");
+    aes_mix_cols(&s);
     aes_print_block(&s);
     printf("\n");
     aes_shift_rows(&s);
-    
     aes_print_block(&s);
     return 0;
 }

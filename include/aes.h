a4db4dbf0923cedf6358566aac81fb92c2ff1b8c
Wallace <wallacj@americas.corpdir.net>
2018-08-17 21:28:12 -0700

    key schedule works! dirty tho...

--- include/aes.h
+++ include/aes.h
@@ -1,6 +1,7 @@
 #ifndef AES_H
 #define AES_H
 #include <inttypes.h>
+#include <stdbool.h>
 
 union block {
     uint8_t bytes[16];
@@ -10,9 +11,12 @@ union block {
 
 struct aes_state {
     /* Maintain 2 state blocks so that operations that can't be done 
-        in place can be done in the other buffer then pointer-swapped*/
+        in place can be done in the other buffer then pointer-swapped */
     union block state[2];
     uint8_t si; // State index. Keeps track of the current state buffer offset
+    uint32_t key_sched[44]; // 4 words of key material * 10+1 rounds
+    uint8_t ks_index; // Current index into key schedule
+    bool debug;
 };
 
 void aes_128_block_encrypt(char *input, char *output, char *key);
@@ -21,13 +25,19 @@ uint8_t s_box(uint8_t s);
 
 uint8_t aes_mult(uint8_t a, uint8_t b);
 
-uint32_t aes_round_constant(unsigned int round);
+uint8_t aes_round_constant(unsigned int round);
 
-uint32_t aes_key_schedule(unsigned int round);
+union block *dref(struct aes_state *state);
+
+void aes_compute_key_schedule(struct aes_state *state, char *key);
+
+void aes_add_round_key(struct aes_state *state);
 
 void aes_sub_bytes(struct aes_state *state);
 
-void aes_shift_rows(struct aes_state * state);
+void aes_shift_rows(struct aes_state *state);
+
+void aes_mix_cols(struct aes_state *state);
 
 void aes_print_block(struct aes_state * state);
 
--- src/aes.c
+++ src/aes.c
@@ -2,15 +2,46 @@
     (c) 2018 Jonathan Wallace */
 #include <stdlib.h>
 #include <inttypes.h>
+#include <string.h>
 #include "aes.h"
 
 
 
 void aes_128_block_encrypt(char *input, char *output, char *key)
 {
-    for (int i = 0; i < 256; i++) {
-        printf("%d, %02hhx\n", i, (2 << (i-1)) % 0x11b);
-    }    
+    struct aes_state s;
+    s.si = 0;
+    s.debug = false;
+
+    memccpy(dref(&s)->bytes, input, sizeof(char), 16);
+
+    if (s.debug) {
+        printf("Input\n");
+        aes_print_block(&s);
+    }
+
+    aes_compute_key_schedule(&s, key);
+
+    aes_add_round_key(&s);
+
+    for (int round = 0; round < 10; round++) {
+        printf("Round %d\n", round);
+        aes_sub_bytes(&s);
+        aes_shift_rows(&s);
+        aes_mix_cols(&s);
+        aes_add_round_key(&s);
+    }
+
+    aes_sub_bytes(&s);
+    aes_shift_rows(&s);
+    aes_add_round_key(&s);
+
+    if (s.debug) {
+        printf("Output\n");
+        aes_print_block(&s);
+    }
+
+    memccpy(output, dref(&s)->bytes, sizeof(char), 16);
     return;
 }
 
@@ -51,7 +82,7 @@ uint8_t aes_mult(uint8_t a, uint8_t b)
     return p;
 }
 
-uint32_t aes_round_constant(unsigned int round)
+uint8_t aes_round_constant(unsigned int round)
 {
     if (round == 0) {
         /* 0x00 is problematic to calculate, but the round constant is 
@@ -63,13 +94,9 @@ uint32_t aes_round_constant(unsigned int round)
     for (round--; round; round--)
         // round constant is just powers of 2 on GF(2^8)
         rcon = aes_mult(rcon, 0x02);
-    return rcon << 24;
+    return rcon;
 }
 
-uint32_t aes_key_schedule(unsigned int round)
-{
-    return 0;
-}
 
 union block *dref(struct aes_state *s)
 {
@@ -77,10 +104,50 @@ union block *dref(struct aes_state *s)
     return &s->state[s->si];
 }
 
+void aes_compute_key_schedule(struct aes_state *s, char *key)
+{
+    memccpy((char *)s->key_sched, key, sizeof(char), 16);
+    for (int i = 4; i < 44; i++) {
+        union {
+            uint32_t word;
+            uint8_t bytes[4];
+        } temp;
+        temp.word = s->key_sched[i - 1];
+        if (i % 4 == 0) {
+            temp.word = temp.word >> 8 | temp.word << 24;
+            for (int j = 0; j < 4; j++)
+                temp.bytes[j] = s_box(temp.bytes[j]);
+            temp.bytes[0] ^= aes_round_constant(i / 4);
+        }
+        s->key_sched[i] = temp.word ^ s->key_sched[i - 4];
+    }
+    for (int i = 0; i < 44; i++) {
+        for( int j = 0; j < 4; j++) {
+            printf("%02hhx", ((char *)s->key_sched)[i*4+j]);
+        }
+        printf("\n");
+    }
+    return;
+}
+
+void aes_add_round_key(struct aes_state * s)
+{
+    if (s->debug) {
+        printf("aes_add_round_key\n");
+        aes_print_block(s);
+    }
+    return;
+}
+
 void aes_sub_bytes(struct aes_state * s)
 {
     for (int i = 0; i < 16; i++)
         dref(s)->bytes[i] = s_box(dref(s)->bytes[i]);
+
+    if (s->debug) {
+        printf("aes_sub_bytes\n");
+        aes_print_block(s);
+    }
 }
 
 void aes_shift_rows(struct aes_state * s)
@@ -96,6 +163,10 @@ void aes_shift_rows(struct aes_state * s)
             end[col*4 + row] = start[row + ((row + col) % 4) * 4];
         }
     }
+    if (s->debug) {
+        printf("aes_shift_rows\n");
+        aes_print_block(s);
+    }
 }
 
 void aes_mix_cols(struct aes_state * s)
@@ -104,7 +175,7 @@ void aes_mix_cols(struct aes_state * s)
     uint8_t *start = dref(s)->bytes;
     s->si = !s->si;
     uint8_t *end = dref(s)->bytes;
-    /* Multiply each column by the static polynomial in GF(2^8) */
+    /* Multiply each column by this static polynomial in GF(2^8) */
     uint8_t mix_cols_poly[4] = {2, 3, 1, 1};
     for (int col = 0; col < 4; col++) {
         for (int row = 0; row < 4; row++) {
@@ -116,32 +187,28 @@ void aes_mix_cols(struct aes_state * s)
             }
         }
     }
+    if (s->debug) {
+        printf("aes_mix_cols\n");
+        aes_print_block(s);
+    }
 }
 
 void aes_print_block(struct aes_state * s)
 {
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            printf("%hhx ", dref(s)->grid[j][i]);
+            printf("%02hhx ", dref(s)->grid[j][i]);
         }
         printf("\n");
     }
+    printf("\n");
 }
 
 int main()
 {
-    struct aes_state s;
-    s.si = 0;
-    for (int i = 0; i < 16; i++) dref(&s)->bytes[i] = 0xd4;
-    dref(&s)->words[0] = 0x4c31262d;
-    dref(&s)->words[1] = 0x455313db;
-    dref(&s)->words[2] = 0x5c220af2;
-    aes_print_block(&s);
-    printf("\n");
-    aes_mix_cols(&s);
-    aes_print_block(&s);
-    printf("\n");
-    aes_shift_rows(&s);
-    aes_print_block(&s);
+    char input[] = "1234567890123456";
+    char key[] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
+    aes_128_block_encrypt(input, input, key);
+    
     return 0;
 }
